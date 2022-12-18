fa5f9ffff7c3956e38e8d1b5b082e9fbe44ba765
HurryNelson <56083283+HurryNelson@users.noreply.github.com>
2022-12-18 17:44:56 -0500

    comment out tests

--- src/test/java/com/midterm/midterm/MidtermApplicationTests.java
+++ src/test/java/com/midterm/midterm/MidtermApplicationTests.java
@@ -24,181 +24,181 @@ import org.apache.http.util.EntityUtils;
 
 @SpringBootTest
 class MidtermApplicationTests {
-	private static int port = 8080;
-	private static URL base;
-	private TestRestTemplate template;
+	// private static int port = 8080;
+	// private static URL base;
+	// private TestRestTemplate template;
 
-	@BeforeAll
-	public static void setUp() throws Exception {
-		base = new URL("http://localhost:" + port + "/");
-	}
+	// @BeforeAll
+	// public static void setUp() throws Exception {
+	// 	base = new URL("http://localhost:" + port + "/");
+	// }
 
-	//test GET
-	@Disabled
-	 void testGet() throws Exception {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=1");
-		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
-		String body = response.getBody();
+	// //test GET
+	// @Disabled
+	//  void testGet() throws Exception {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=1");
+	// 	ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+	// 	String body = response.getBody();
 
-		ObjectMapper mapper = new ObjectMapper();
-		Food[] foodJson = mapper.readValue(body, Food[].class);
+	// 	ObjectMapper mapper = new ObjectMapper();
+	// 	Food[] foodJson = mapper.readValue(body, Food[].class);
 
-		String resultText = foodJson[0].getFoodName();
+	// 	String resultText = foodJson[0].getFoodName();
 
-		assertEquals("BLT", resultText);
-	}
+	// 	assertEquals("BLT", resultText);
+	// }
 
-	//test GET all
-	@Disabled
-	void testGetAll() throws Exception {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=0");
-		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
-		String body = response.getBody();
+	// //test GET all
+	// @Disabled
+	// void testGetAll() throws Exception {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=0");
+	// 	ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+	// 	String body = response.getBody();
 
-		ObjectMapper mapper = new ObjectMapper();
-		Food[] foodJson = mapper.readValue(body, Food[].class);
+	// 	ObjectMapper mapper = new ObjectMapper();
+	// 	Food[] foodJson = mapper.readValue(body, Food[].class);
 
-		String resultText = foodJson[0].getFoodName();
-		String resultText2 = foodJson[1].getFoodName();
+	// 	String resultText = foodJson[0].getFoodName();
+	// 	String resultText2 = foodJson[1].getFoodName();
 
-		assertTrue(resultText.contains("BLT"), "BLT was not returned:" + resultText);
-		assertTrue(resultText2.contains("Chicken"), "Chicken was not returned:" + resultText2);
-	}
+	// 	assertTrue(resultText.contains("BLT"), "BLT was not returned:" + resultText);
+	// 	assertTrue(resultText2.contains("Chicken"), "Chicken was not returned:" + resultText2);
+	// }
 
-	//test GET error
-	@Disabled
-	void testGetError() throws Exception {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=3");
-		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
-		String body = response.getBody();
+	// //test GET error
+	// @Disabled
+	// void testGetError() throws Exception {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=3");
+	// 	ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+	// 	String body = response.getBody();
 
-		ObjectMapper mapper = new ObjectMapper();
-		Food[] foodJson = mapper.readValue(body, Food[].class);
+	// 	ObjectMapper mapper = new ObjectMapper();
+	// 	Food[] foodJson = mapper.readValue(body, Food[].class);
 
-		String resultText = foodJson[0].getFoodName();
+	// 	String resultText = foodJson[0].getFoodName();
 
-		assertEquals("No food here", resultText);
-	}
+	// 	assertEquals("No food here", resultText);
+	// }
 
-	//test POST
-	@Disabled
-	void testPost() throws Exception {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=4&foodName=hotDog&calories=115&fat=0.6&carbs=37");
-		ResponseEntity<String> response = template.postForEntity(base.toString(), String.class, String.class, String.class);
-		String body = response.getBody();
+	// //test POST
+	// @Disabled
+	// void testPost() throws Exception {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=4&foodName=hotDog&calories=115&fat=0.6&carbs=37");
+	// 	ResponseEntity<String> response = template.postForEntity(base.toString(), String.class, String.class, String.class);
+	// 	String body = response.getBody();
 
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=4");
-		response = template.getForEntity(base.toString(), String.class);
-		body = response.getBody();
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=4");
+	// 	response = template.getForEntity(base.toString(), String.class);
+	// 	body = response.getBody();
 
-		ObjectMapper mapper = new ObjectMapper();
-		Food[] foodJson = mapper.readValue(body, Food[].class);
+	// 	ObjectMapper mapper = new ObjectMapper();
+	// 	Food[] foodJson = mapper.readValue(body, Food[].class);
 
-		String resultText = foodJson[0].getFoodName();		
+	// 	String resultText = foodJson[0].getFoodName();		
 
-		assertEquals("hotDog", resultText);
-	}
+	// 	assertEquals("hotDog", resultText);
+	// }
 
-	//test POST error
-	@Disabled
-	void testPostError() throws Exception {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=1&foodName=hotDog&calories=115&fat=0.6&carbs=37");
-		ResponseEntity<String> response = template.postForEntity(base.toString(), String.class, String.class, String.class);
-		String body = response.getBody();
+	// //test POST error
+	// @Disabled
+	// void testPostError() throws Exception {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=1&foodName=hotDog&calories=115&fat=0.6&carbs=37");
+	// 	ResponseEntity<String> response = template.postForEntity(base.toString(), String.class, String.class, String.class);
+	// 	String body = response.getBody();
 
-		assertTrue(body.contains("There is already food here, cannot add a new order"), body + " was returned");	
-	}
+	// 	assertTrue(body.contains("There is already food here, cannot add a new order"), body + " was returned");	
+	// }
 
-	//test PUT
-	@Disabled
-	void testPut() throws Exception  {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=2&sauce=BBQ");
-		ResponseEntity<String> response = template.exchange(base.toString(), HttpMethod.PUT, null, String.class);
-		String body = response.getBody();
+	// //test PUT
+	// @Disabled
+	// void testPut() throws Exception  {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=2&sauce=BBQ");
+	// 	ResponseEntity<String> response = template.exchange(base.toString(), HttpMethod.PUT, null, String.class);
+	// 	String body = response.getBody();
 
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=2");
-		response = template.getForEntity(base.toString(), String.class);
-		body = response.getBody();
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=2");
+	// 	response = template.getForEntity(base.toString(), String.class);
+	// 	body = response.getBody();
 
-		ObjectMapper mapper = new ObjectMapper();
-		Food[] foodJson = mapper.readValue(body, Food[].class);
+	// 	ObjectMapper mapper = new ObjectMapper();
+	// 	Food[] foodJson = mapper.readValue(body, Food[].class);
 
-		String resultText = foodJson[0].getFoodName();
+	// 	String resultText = foodJson[0].getFoodName();
 
-		assertTrue(resultText.contains("BBQ"), "BBQ sauce was not added to the chicken:" + resultText);
-	}
+	// 	assertTrue(resultText.contains("BBQ"), "BBQ sauce was not added to the chicken:" + resultText);
+	// }
 
-	//test PUT errors
-	@Disabled
-	void testPutNoFoodError() throws Exception  {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=3&sauce=BBQ");
-		ResponseEntity<String> response = template.exchange(base.toString(), HttpMethod.PUT, null, String.class);
-		String body = response.getBody();
+	// //test PUT errors
+	// @Disabled
+	// void testPutNoFoodError() throws Exception  {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=3&sauce=BBQ");
+	// 	ResponseEntity<String> response = template.exchange(base.toString(), HttpMethod.PUT, null, String.class);
+	// 	String body = response.getBody();
 
-		assertTrue(body.contains("There is no food, cannot add a sauce"), body + " was returned");
-	}
+	// 	assertTrue(body.contains("There is no food, cannot add a sauce"), body + " was returned");
+	// }
 	
-	@Disabled
-	void testPutNoSauceError() throws Exception  {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=1&sauce=");
-		ResponseEntity<String> response = template.exchange(base.toString(), HttpMethod.PUT, null, String.class);
-		String body = response.getBody();
+	// @Disabled
+	// void testPutNoSauceError() throws Exception  {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=1&sauce=");
+	// 	ResponseEntity<String> response = template.exchange(base.toString(), HttpMethod.PUT, null, String.class);
+	// 	String body = response.getBody();
 
-		assertTrue(body.contains("There is no sauce, cannot add a sauce"), body + " was returned");
-	}
+	// 	assertTrue(body.contains("There is no sauce, cannot add a sauce"), body + " was returned");
+	// }
 
-	@Disabled
-	void testPutSaucedAlreadyError() throws Exception  {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=1&sauce=BBQ");
-		ResponseEntity<String> response = template.exchange(base.toString(), HttpMethod.PUT, null, String.class);
-		String body = response.getBody();
+	// @Disabled
+	// void testPutSaucedAlreadyError() throws Exception  {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=1&sauce=BBQ");
+	// 	ResponseEntity<String> response = template.exchange(base.toString(), HttpMethod.PUT, null, String.class);
+	// 	String body = response.getBody();
 
-		assertTrue(body.contains("There is already sauce on the food, cannot add a sauce"), body + " was returned");
-	}
+	// 	assertTrue(body.contains("There is already sauce on the food, cannot add a sauce"), body + " was returned");
+	// }
 
-	//test DELETE
-	@Disabled
-	void testDelete() throws Exception {
-		template = new TestRestTemplate();
-		this.base = new URL(this.base.toString() + "food?orderNum=4");
-		template.delete(base.toString(), String.class);
-		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
-		String body = response.getBody();
+	// //test DELETE
+	// @Disabled
+	// void testDelete() throws Exception {
+	// 	template = new TestRestTemplate();
+	// 	this.base = new URL(this.base.toString() + "food?orderNum=4");
+	// 	template.delete(base.toString(), String.class);
+	// 	ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+	// 	String body = response.getBody();
 
-		ObjectMapper mapper = new ObjectMapper();
-		Food[] foodJson = mapper.readValue(body, Food[].class);
+	// 	ObjectMapper mapper = new ObjectMapper();
+	// 	Food[] foodJson = mapper.readValue(body, Food[].class);
 
-		String resultText = foodJson[0].getFoodName();
+	// 	String resultText = foodJson[0].getFoodName();
 
-		assertEquals("No food here", resultText);
-	}
+	// 	assertEquals("No food here", resultText);
+	// }
 
-	//test DELETE error
-	@Disabled
-	void testDeleteError() {
-		CloseableHttpClient client = HttpClients.createDefault();
-		String expectedCode = "HTTP/1.1 404 ";
-		String actualCode = "";
+	// //test DELETE error
+	// @Disabled
+	// void testDeleteError() {
+	// 	CloseableHttpClient client = HttpClients.createDefault();
+	// 	String expectedCode = "HTTP/1.1 404 ";
+	// 	String actualCode = "";
 
-		try {
-			HttpGet request = new HttpGet("http://localhost:8080/food?orderNum=5");
-			CloseableHttpResponse response = client.execute(request);
-			actualCode = response.getStatusLine().toString();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	// 	try {
+	// 		HttpGet request = new HttpGet("http://localhost:8080/food?orderNum=5");
+	// 		CloseableHttpResponse response = client.execute(request);
+	// 		actualCode = response.getStatusLine().toString();
+	// 	} catch (Exception e) {
+	// 		e.printStackTrace();
+	// 	}
 
-		assertEquals(expectedCode, actualCode);
-	}
+	// 	assertEquals(expectedCode, actualCode);
+	// }
 }
